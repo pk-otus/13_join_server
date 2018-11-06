7a161d6d533643d63c79b13b567c7a13417d892f
Paul <a@a.a>
2018-11-06 22:28:24 +0300

    port in command line

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 
 int main(int argc, char const *argv[]) 
 {
-	join_server::server q(9999);
+	if (argc < 2)
+	{
+		std::cout << "Usage: join_server <server port>\n";
+		return 1;
+	}
+	size_t port = std::stoi(argv[1]);
+	
+	join_server::server q(port);
 
 	while (true) 
 	{
